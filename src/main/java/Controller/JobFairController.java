4d0deeba4b1b46d1ffb4a99cf70ba6ee526ef97b
rynne4976-art <rynne4976@gmail.com>
2026-03-10 09:54:20 +0900

    Scholar 최초 기능 구현 (#8)
    * chore: remove jobfair files from scholar branch
    
    * feat: add scholar search feature

--- src/main/java/Controller/JobFairController.java
+++ src/main/java/Controller/JobFairController.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import Service.JobFairService;
 
-@WebServlet("/jobfair/*") 
+@WebServlet("/jobfair/*")
 public class JobFairController extends HttpServlet {
     private static final long serialVersionUID = 1L;
 

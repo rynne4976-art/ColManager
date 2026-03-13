b8d4ec9b52dca9bbdd72963c4895d81bda1f881f
juwon-07 <jjo072020@gmail.com>
2026-03-13 09:36:34 +0900

    Bus (#18)
    * bus.css,bus.js 제작 및 startcenter 버스 시간표 부분 수정
    
    * bus.js 제거 (버스 기능 수정)
    
    * BusService.java 수정! 및 bus.css 수정
    
    * bus.css,BusController.java,BusService.java 최종 수정
    
    * bottom.jsp
    <!-- 저작권 및 학교 이름 ----- -->
    <div class="col-md-4 text-center text-md-start mb-3 mb-md-0">
    주석 내용 추가
    
    * bus.js generate, modify buscontroller, busservice, bus.css
    
    * modify buscontroller, bus.js, busservice
    
    ---------
    
    Co-authored-by: rynne4976-art <rynne4976@gmail.com>

--- src/main/java/Controller/BusController.java
+++ src/main/java/Controller/BusController.java
@@ -7,7 +7,6 @@ import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
-import java.sql.*;
 import java.util.*;
 
 @WebServlet("/bus")
@@ -18,46 +17,20 @@ public class BusController extends HttpServlet {
 
         BusService service = new BusService();
 
-        List<Map<String,String>> schedule = service.getSchedule();
+        // 버스 시간표
+        List<Map<String, String>> schedule = service.getSchedule();
 
-        List<String> buildings = new ArrayList<>();
+        // 건물 목록
+        List<String> buildings = service.getBuildings();
 
-        try{
-
-            Class.forName("com.mysql.cj.jdbc.Driver");
-
-            Connection conn =
-            DriverManager.getConnection(
-            "jdbc:mysql://localhost:3306/colbus",
-            "root",
-            "0000");
-
-            String sql = "SELECT name FROM building";
-
-            PreparedStatement ps = conn.prepareStatement(sql);
-
-            ResultSet rs = ps.executeQuery();
-
-            while(rs.next()){
-                buildings.add(rs.getString("name"));
-            }
-
-            rs.close();
-            ps.close();
-            conn.close();
-
-        }catch(Exception e){
-            e.printStackTrace();
-        }
-
-        Map<String,Object> result = new HashMap<>();
-
+        Map<String, Object> result = new HashMap<>();
         result.put("schedule", schedule);
         result.put("buildings", buildings);
 
         response.setContentType("application/json;charset=UTF-8");
 
-        String json = new Gson().toJson(result);
+        Gson gson = new Gson();
+        String json = gson.toJson(result);
 
         response.getWriter().print(json);
     }
